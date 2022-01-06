72f6ae1f64293b2143a64513a0bdfa68dace6880
hiroto302 <i.am.a.hiroto@gmail.com>
2022-01-07 01:27:34 +0900

    3人称視点操作 player の回転移動機能を追加
    移動に合わせて player の正面方向を回転させるメソッド

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,10 +17,13 @@ public class PlayerController : MonoBehaviour
     // 現在位置からの相対的な移動位置
     Vector3 movementPosition;
 
+    // 現在の位置
+    Vector3 currentPosition;
+
 
     void Start()
     {
-        
+        currentPosition = transform.position;
     }
 
     void Update()
@@ -31,6 +34,7 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         MovePlayerPosintion(movementPosition);
+        RotateMovementDirection();
     }
 
     // player の移動位置を取得
@@ -46,5 +50,23 @@ public class PlayerController : MonoBehaviour
     void MovePlayerPosintion(Vector3 movementPosition)
     {
         rb.velocity = this.movementPosition;
+    }
+
+    // 3人称視点操作に合わせた player の回転移動
+    // 移動に合わせて player の正面方向を回転させるメソッド
+    void RotateMovementDirection()
+    {
+        // 移動方向ベクトルの取得( 回転させる方向)
+        Vector3 directionVector = new Vector3(transform.position.x, 0, transform.position.z) - new Vector3(currentPosition.x, 0, currentPosition.z);
+        // 移動後の現在位置を更新
+        currentPosition = transform.position;
+        // 一定距離移動した場合回転
+        if (Mathf.Abs(directionVector.x) > 0.001f || Mathf.Abs(directionVector.z) > 0.001f)
+        {
+            // 回転量を取得
+            Quaternion rot = Quaternion.LookRotation(directionVector);
+            // 緩やかに回転
+            this.transform.rotation = Quaternion.Slerp(rb.transform.rotation, rot, 0.1f);
+        }
     }
 }
