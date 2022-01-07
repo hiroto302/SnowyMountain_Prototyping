6164940a5c5d3c855ee84822924bb2017d982cd9
hiroto302 <i.am.a.hiroto@gmail.com>
2022-01-07 22:10:41 +0900

    マウスカーソルの動きに合わせた視点移動を実装

--- Assets/Scripts/FirstPersonPerspectiveController.cs
+++ Assets/Scripts/FirstPersonPerspectiveController.cs
@@ -13,13 +13,22 @@ public class FirstPersonPerspectiveController : MonoBehaviour
     [SerializeField]
     Camera viewpointCamera = null;
 
-    public float rotationSpeed;
+    // MoveViewPointメソッドで利用する変数群
+    // 回転速度
+    public float RotateSpeed = 2.0f;
+    // X, Y軸 に対する現在の回転角度
+    float rotationAngleX, rotationAngleY;
+    // 回転初期値
+    Quaternion initialRotation;
+    // X, Y軸 に対する回転角度の初期値
+    Vector3 initialRotationAngle;
+    // 上下に向ける角度(回転)を制限
+    float maxRotationAngleX = 60.0f;
 
+    // DragMoveViewPointメソッドで使用する変数群
     Vector3 lastMousePosition;
     Vector3 newAngle = Vector3.zero;
 
-
-    public float rotateSpeed = 2.0f;  //回転の速さ
 
     void Reset()
     {
@@ -30,14 +39,49 @@ public class FirstPersonPerspectiveController : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        GetInitialRotation();
+    }
+
     void Update()
     {
-        DragMoveViewpoint();
+        MoveViewPoint();
+        // DragMoveViewpoint();
     }
 
     // 視点をマウスの合わせて制御できるようにする
+    void MoveViewPoint()
+    {
+        // 左右方向
+        rotationAngleY += Input.GetAxis("Mouse X") * RotateSpeed;
+
+        // 上下方向
+        rotationAngleX -= Input.GetAxis("Mouse Y") * RotateSpeed;
+        // 上下方向の制限
+        if(rotationAngleX > maxRotationAngleX)
+        {
+            rotationAngleX = maxRotationAngleX;
+        }
+        else if (rotationAngleX < -maxRotationAngleX)
+        {
+            rotationAngleX = -maxRotationAngleX;
+        }
+        // カメラの回転
+        transform.rotation = Quaternion.Euler(rotationAngleX, rotationAngleY, 0);
+    }
+
+    void GetInitialRotation()
+    {
+        initialRotation = this.transform.rotation;
+        initialRotationAngle = initialRotation.eulerAngles;
+        rotationAngleX = initialRotationAngle.x;
+        rotationAngleY = initialRotationAngle.y;
+    }
+
 
     // マウスの左クリック中、ドラッグした時視点を移動するメソッド
+    // マウスのクリック位置を取得することでカメラの回転を実装
     void DragMoveViewpoint()
     {
         // 左クリックをした時
@@ -61,5 +105,4 @@ public class FirstPersonPerspectiveController : MonoBehaviour
             lastMousePosition = Input.mousePosition;
         }
     }
-
 }
