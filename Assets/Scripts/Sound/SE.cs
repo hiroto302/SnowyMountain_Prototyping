1fc361b8e5beb8ed82174ab5a3209368bd664d1e
hiroto302 <i.am.a.hiroto@gmail.com>
2022-01-17 15:58:08 +0900

    SEを制御するスクリプトを作成

---
+++ Assets/Scripts/Sound/SE.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 各オブジェクトのSEを制御するスクリプトコンポーネント
+[RequireComponent(typeof(AudioSource))]
+public class SE : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource source = null;
+    [SerializeField]
+    AudioClip[] sound = null;
+
+    void Reset()
+    {
+        source = GetComponent<AudioSource>();
+        SetDefault();
+    }
+
+
+    // AudioSource のデフォルト設定
+    void SetDefault()
+    {
+        source.priority = 128;
+        source.volume = 0.3f;
+        source.loop = false;
+        source.spatialBlend = 1;
+        source.playOnAwake = false;
+    }
+
+    // 指定した音を鳴らすメソッド
+    public void PlaySE(int n)
+    {
+        source.PlayOneShot(sound[n], source.volume);
+    }
+    public void PlaySE(int n, float volume)
+    {
+        source.PlayOneShot(sound[n], volume);
+    }
+    public void PlaySE(int n, float volume, Vector3 position)
+    {
+        AudioSource.PlayClipAtPoint(sound[n], position, volume);
+    }
+}
