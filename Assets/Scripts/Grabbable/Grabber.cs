4cb9d80eba5a721b04f53381e798045d5a3bee54
hiroto302 <i.am.a.hiroto@gmail.com>
2022-01-19 23:38:50 +0900

    Create Grabber.cs
    Player の手を構成するスクリプトコンポーネント
    
    GrabbabeObject と合わせて動作する

---
+++ Assets/Scripts/Grabbable/Grabber.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// player の物を掴む手にアタッチ
+public class Grabber : MonoBehaviour
+{
+    // 右手 or 左手
+    public enum Hand { R, L };
+    [SerializeField] Hand hand = Hand.R;
+
+    // 物を掴んでいる状態か
+    public enum State { Normal, Grabbing };
+    public State state = State.Normal;
+
+    // 現在掴んでいる物の名前
+    public static string GrabbingObjectName = null;
+    // 掴んでいるオブジェクトのクラスコンポーネント
+    public GrabbableObject grabbable = null;
+
+    void Start()
+    {
+        GrabbableObject.OnGrabbed += GrabObject;
+    }
+
+    // インタラクトして、GrabbableObject のスクリプトを持つなら手元へ移動した時に呼ばれる
+    public void GrabObject()
+    {
+        grabbable = GetComponentInChildren<GrabbableObject>();
+        SetState(State.Grabbing);
+        GetObjectName(grabbable.ObjectName);
+    }
+    // 掴んでいるものを離す
+    public void ReleaseGrabbedObject()
+    {
+        grabbable.DetachFromHand();
+        SetState(State.Normal);
+        GrabbingObjectName = null;
+    }
+    // 状態を変更するメソッド
+    public void SetState(State state)
+    {
+        this.state = state;
+    }
+    // 掴んでいるオブジェクト名を取得
+    public void GetObjectName(string name)
+    {
+        GrabbingObjectName = name;
+    }
+}
