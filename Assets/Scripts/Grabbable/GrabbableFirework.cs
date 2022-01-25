cbc815f88a30e7c2ed4fc3b05cb6438c675f822e
hiroto302 <i.am.a.hiroto@gmail.com>
2022-01-26 05:58:06 +0900

    Update GrabbableFirework.cs
    炎に入れらた時の処理を追加

--- Assets/Scripts/Grabbable/GrabbableFirework.cs
+++ Assets/Scripts/Grabbable/GrabbableFirework.cs
@@ -6,8 +6,12 @@ using System;
 // このスクリプトをアタッチしたオブジェクトを火の中に入れると花火が打ち上がる
 public class GrabbableFirework : GrabbableObject
 {
+    // 花火の源
+    [SerializeField] GameObject fireworks = null;
+    [SerializeField] Transform shootposition = null;
     // 焚火に入れらて燃やされた時に発生する event
     public static event Action OnIgniteFirework;
+
     public override void Start()
     {
         base.Start();
@@ -19,7 +23,11 @@ public class GrabbableFirework : GrabbableObject
     {
         if(currentState == State.Grabbed)
         {
-            OnIgniteFirework();
+            if(OnIgniteFirework != null)
+                OnIgniteFirework();
+
+            GameObject fireworksObject =  Instantiate(fireworks, shootposition);
+            fireworksObject.GetComponent<FireworksController>().ShootFireworks();
             Grabber grabber = GetComponentInParent<Grabber>();
             grabber.ReleaseGrabbedObject();
             Destroy(this.gameObject);
