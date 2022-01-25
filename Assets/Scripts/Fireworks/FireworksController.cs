0ae8d78d120039a0aadeb237fc1c345b5f565699
hiroto302 <i.am.a.hiroto@gmail.com>
2022-01-26 05:57:22 +0900

    Create FireworksController : セットした花火をうちあげる機能

---
+++ Assets/Scripts/Fireworks/FireworksController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// セットしてある Firework を打ち上げるクラス
+public class FireworksController : MonoBehaviour
+{
+    [SerializeField] List<GameObject> fireworks = null;
+    Coroutine shootCoroutine;
+    // 花火を打ち上げるメソッド
+    public void ShootFireworks()
+    {
+        shootCoroutine = StartCoroutine(ShootRoutine());
+        Debug.Log("打つよ");
+    }
+
+    // セットされている花火を生成
+    public IEnumerator ShootRoutine()
+    {
+        yield return null;
+        int i = 0;
+        while(fireworks.Count > i)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(0.5f, 1.0f));
+            fireworks[i].GetComponent<ParticleSystem>().Play();
+            i++;
+        }
+        StopCoroutine(shootCoroutine);
+        shootCoroutine = null;
+        // Destroy(this.gameObject);
+    }
+}
