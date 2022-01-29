4c91ca83987c57abe263a3ddeffe2d963c9708cb
hiroto302 <i.am.a.hiroto@gmail.com>
2022-01-29 12:33:01 +0900

    updaate OnTriggerEvent
    コルーチンで再度実行できるまでの時間を計測するようにした

--- Assets/Scripts/OnTriggerEvent.cs
+++ Assets/Scripts/OnTriggerEvent.cs
@@ -2,69 +2,103 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-// このカスタムスクリプトコンポーネントは、
+
+// // このカスタムスクリプトコンポーネントは、
 // トリガーとして設定されたコライダーによって定義されたエリアにユーザーが入ったり出たりしたときに起こる1つまたは複数のイベントを指定できる
 // Is TriggerプロパティがそのColliderコンポーネントに対して有効になっている場合にのみ動作
-
+// event を毎回 or 一度 or 時間間隔で呼ぶことが設定できる
 [RequireComponent(typeof(Collider))]
 public class OnTriggerEvent : MonoBehaviour
 {
-    // 侵入した時
     [Header("Trigger Enter Event Section")]
-    public bool EnterIsOneShot;
-    public float EnterEventCooldown;
-    public UnityEvent OnTriggerEnterEvent;
+    [SerializeField] bool enterIsOneShot;       // 一度実行するのみか
+    [SerializeField] float enterEventCooldown;  // 次に実行可能にするまでの時間
+    [SerializeField] UnityEvent onTriggerEnterEvent = null;
 
-    // 出た時
+    [Space]
     [Header("Trigger Exit Event Section")]
-    public bool ExitIsOneShot;
-    public float ExitEventCooldown;
-    public UnityEvent OnTriggerExitEvent;
+    [SerializeField] bool exitIsOneShot;
+    [SerializeField] float exitEventCooldown;
+    [SerializeField] UnityEvent onTriggerExitEvent = null;
 
-    bool enterHasBeenTriggered;
-    float enterTimer;
-
-    bool exitHasBeenTriggered;
+    bool enterHasBeenTrigged;                   // 既に実行したか
+    float enterTimer;                           // 侵入してからの経過時間
+    Coroutine enterTimerCoroutine;
+    bool exitHasBeenTrigged;
     float exitTimer;
+    Coroutine exitTimerCoroutine;
 
     void Start()
     {
-        enterTimer = EnterEventCooldown;
-        exitTimer = ExitEventCooldown;
+        // 最初の１度目を実行できるようにするため
+        enterTimer = enterEventCooldown;
+        exitTimer = exitEventCooldown;
     }
 
-    void Update()
-    {
-        if(enterHasBeenTriggered)
-            enterTimer += Time.deltaTime;
 
-        if(exitHasBeenTriggered)
-            exitTimer += Time.deltaTime;
-    }
-
+    // 侵入した時
     void OnTriggerEnter(Collider other)
     {
-        if(EnterIsOneShot && enterHasBeenTriggered)
+        if(onTriggerEnterEvent == null)
             return;
 
-        if(EnterEventCooldown > enterTimer)
+        if(enterIsOneShot && enterHasBeenTrigged)
             return;
 
-        OnTriggerEnterEvent.Invoke();
-        enterHasBeenTriggered = true;
-        exitTimer = 0f;
+        if(enterEventCooldown > enterTimer)
+            return;
+
+        onTriggerEnterEvent.Invoke();
+        enterHasBeenTrigged = true;
+
+        if(enterIsOneShot == false && enterTimerCoroutine == null)
+            enterTimerCoroutine = StartCoroutine(CountEnterCoolDownTimeRoutine());
     }
 
+    // 出た時
     void OnTriggerExit(Collider other)
     {
-        if(ExitIsOneShot && exitHasBeenTriggered)
+        if(onTriggerExitEvent == null)
             return;
 
-        if(ExitEventCooldown > exitTimer)
+        if(exitIsOneShot && exitHasBeenTrigged)
             return;
 
-        OnTriggerExitEvent.Invoke();
-        exitHasBeenTriggered = true;
-        exitTimer = 0f;
+        if(exitEventCooldown > exitTimer)
+            return;
+
+        onTriggerExitEvent.Invoke();
+        exitHasBeenTrigged = true;
+
+        if(exitIsOneShot == false && exitTimerCoroutine == null)
+            exitTimerCoroutine = StartCoroutine(CountExitCoolDownTimeRoutine());
+
+    }
+
+    // クールダウンを計測するルーティーン
+    IEnumerator CountEnterCoolDownTimeRoutine()
+    {
+        // 初期化
+        enterTimer = 0;
+        float waitTime = 0.1f;
+        while(enterEventCooldown > enterTimer)
+        {
+            enterTimer += waitTime;
+            yield return new WaitForSeconds(waitTime);
+        }
+        StopCoroutine(enterTimerCoroutine);
+        enterTimerCoroutine = null;
+    }
+    IEnumerator CountExitCoolDownTimeRoutine()
+    {
+        exitTimer = 0;
+        float waitTime = 0.1f;
+        while(exitEventCooldown > exitTimer)
+        {
+            exitTimer += waitTime;
+            yield return new WaitForSeconds(waitTime);
+        }
+        StopCoroutine(exitTimerCoroutine);
+        exitTimerCoroutine = null;
     }
 }
