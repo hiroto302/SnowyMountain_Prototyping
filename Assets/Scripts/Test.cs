a79407060bdb8d3e3781d4ae77adb265c82c8d61
hiroto302 <i.am.a.hiroto@gmail.com>
2022-01-31 15:21:14 +0900

    Create Test.cs

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+// テスト用クラス
+public class Test : MonoBehaviour
+{
+
+    // 使用されている GraphicsAPI
+    [SerializeField] TextMeshProUGUI textGAPI = null;
+    string graphicsAPI;
+
+    // 経過時間を測定
+    Coroutine timerCoroutine;
+    [SerializeField] TextMeshProUGUI elapsedTime = null;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            ExecuteTest();
+        }
+    }
+
+    void ExecuteTest()
+    {
+        Debug.Log(SystemInfo.graphicsDeviceType + " graphics API type ");
+        graphicsAPI = SystemInfo.graphicsDeviceType.ToString();
+        textGAPI.text = graphicsAPI;
+
+        TestCoroutine();
+    }
+
+    void TestCoroutine()
+    {
+        timerCoroutine =  StartCoroutine(CountTimeRoutine());
+        Debug.Log("Start Coroutine");
+    }
+    IEnumerator CountTimeRoutine()
+    {
+        float elapsed = 0;
+        float max = 10.0f;
+        float waitTime = 1.0f;
+        while(max > elapsed)
+        {
+            elapsed += waitTime;
+            yield return new WaitForSeconds(waitTime);
+            Debug.Log(elapsed + " : elapsedTime");
+            elapsedTime.text = elapsed.ToString();
+        }
+        Debug.Log(" End Coroutine");
+        StopCoroutine(timerCoroutine);
+        timerCoroutine = null;
+    }
+}
